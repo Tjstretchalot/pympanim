8184b16a78c67b258dce1cfe77361b19794403f0
Timothy Moore <mtimothy984@gmail.com>
2019-07-26 11:02:38 -0700

    increment minor version because of readme change

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setuptools.setup(
     name='pympanim',
-    version='0.0.1',
+    version='0.0.2',
     author='Timothy Moore',
     author_email='mtimothy984@gmail.com',
     description='Multiprocessing-friendly animations',

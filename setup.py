9872d8890c8128f6e4f1ea77684295a7cf7b6f92
Timothy Moore <mtimothy984@gmail.com>
2020-10-13 17:19:34 -0700

    Fix missing dependency in setup.py, bump to 0.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setuptools.setup(
     name='pympanim',
-    version='0.0.7',
+    version='0.0.8',
     author='Timothy Moore',
     author_email='mtimothy984@gmail.com',
     description='Multiprocessing-friendly animations',
@@ -15,11 +15,12 @@ setuptools.setup(
     long_description=open(
         os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     long_description_content_type='text/markdown',
-    install_requires=['pyzmq', 'pytypeutils', 'Pillow'],
+    install_requires=['pyzmq', 'pytypeutils', 'Pillow', 'psutil'],
     classifiers=(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
         'Topic :: Utilities'),
     python_requires='>=3.6',

eb38d934e96a577b0b4d73b70edb30b53bf0e4ce
Timothy Moore <mtimothy984@gmail.com>
2024-09-23 13:22:22 -0700

    Fix missing comma in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.8',
         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
         'Topic :: Utilities'),
     python_requires='>=3.6',
